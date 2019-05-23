9f3704bcf4afdcceeafacb0338e0a817b572aeb4
tsid14 <tsid14@lm.lmig.com>
2019-05-23 09:46:30 -0400

    Converted RadioButton to a functional component

--- spec/RadioButtonSpec.js
+++ spec/RadioButtonSpec.js
@@ -1,11 +1,12 @@
 import RadioButton from '../src/components/RadioButton.js';
+import TestWrapper from '../src/components/TestWrapper.js';
 import React from 'react';
 import TestUtils from 'react-dom/test-utils';
 
 describe('RadioButton', function() {
     let component;
     beforeEach(() => {
-        component = TestUtils.renderIntoDocument(<RadioButton label='answer' />);
+        component = TestUtils.renderIntoDocument(<TestWrapper><RadioButton label='answer' /></TestWrapper>);
     });
 
     it ('renders without problems', function(){
--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,33 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const RadioButton = (props) => {
+    const handleChange = () => props.onChange(props.value);
 
-class RadioButton extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    handleChange(){
-        this.props.onChange(this.props.value);
-    }
-
-    render() {
-        return (
-            <div className='radio'>
-                <label>
-                    <input
-                        type='radio'
-                        name={this.props.name}
-                        value={this.props.value}
-                        checked={this.props.checked}
-                        onChange={this.handleChange.bind(this)}
-                    />
-                    <span>{this.props.label}</span>
-                </label>
-            </div>
-        );
-    }
-}
+    return (
+        <div className='radio'>
+            <label>
+                <input
+                    type='radio'
+                    name={props.name}
+                    value={props.value}
+                    checked={props.checked}
+                    onChange={handleChange}
+                />
+                <span>{props.label}</span>
+            </label>
+        </div>
+    );
+};
+//
+// class RadioButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     handleChange(){
+//         this.props.onChange(this.props.value);
+//     }
+//
+//     render() {
+//         return (
+//             <div className='radio'>
+//                 <label>
+//                     <input
+//                         type='radio'
+//                         name={this.props.name}
+//                         value={this.props.value}
+//                         checked={this.props.checked}
+//                         onChange={this.handleChange.bind(this)}
+//                     />
+//                     <span>{this.props.label}</span>
+//                 </label>
+//             </div>
+//         );
+//     }
+// }
 
 RadioButton.propTypes = {
     name: PropTypes.string,
